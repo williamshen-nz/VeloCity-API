e5563651bc09e4a21ee4464ab300333aa2d3b94f
manalmohania <u6019071@anu.edu.au>
2017-07-29 12:11:56 +1000

    add crashes to the list from CSV

--- src/main/java/nz/shen/velocity/velocity/CycleCrashes.java
+++ src/main/java/nz/shen/velocity/velocity/CycleCrashes.java
@@ -1,5 +1,10 @@
 package nz.shen.velocity.velocity;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CycleCrashes {
@@ -33,7 +38,7 @@ public class CycleCrashes {
         }
     }
 
-    private List<CycleCrash> crashes;
+    private List<CycleCrash> crashes = new ArrayList<>();
 
     public List<CycleCrash> getCrashes() {
         return crashes;
@@ -41,5 +46,47 @@ public class CycleCrashes {
 
     public void addCrash(String date, String severity, double latitude, double longitude) {
         crashes.add(new CycleCrash(date, severity, latitude, longitude));
+    }
+
+    private void readCrashesCSV(){
+        String csvFile = "data/Cyclist_Crashes.csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = "\t";
+
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            boolean first = true;
+            while ((line = br.readLine()) != null) {
+
+                if (first) {
+                    first = false;
+                    continue;
+                }
+                // use comma as separator
+                String[] crash = line.split(cvsSplitBy);
+
+                addCrash(crash[0], crash[1], Double.parseDouble(crash[6]), Double.parseDouble(crash[7]));
+
+            }
+
+        }  catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new CycleCrashes().readCrashesCSV();
     }
 }
